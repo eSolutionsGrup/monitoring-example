05be340471a565a4486fc662489d11e683e8dcfa
Marius Staicu <marius.staicu@esolutions.ro>
2023-07-03 13:16:21 +0300

    - more stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # monitoring-example
 Demo project for Monitoring
+
+
+```bash
+watch -n 1 http -a user:password monitoring-demo.localhost/
+```
+
+```bash
+watch http -a user:password monitoring-demo.localhost/hello
+```
+
+```bash
+for counter in {1..5}; do http -a user:password monitoring-demo.localhost/db &; done
+```
---
+++ src/main/kotlin/ro/esolutions/monitoringexample/MonitoringExampleApplication.kt
@@ -0,0 +1,11 @@
+package ro.esolutions.monitoringexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitoringExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonitoringExampleApplication>(*args)
+}
